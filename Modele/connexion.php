2847bc85b74be20df79eb9b8c0cb175295dca030
NicolasH-MDS <nicolas.hubert@my-digital-school.org>
2025-09-17 11:43:00 +0200

    Connexion à la bdd

---
+++ Modele/connexion.php
@@ -0,0 +1,15 @@
+<?php
+$host = 'localhost'; // ou 127.0.0.1
+$dbname = 'workshop';
+$username = 'root';
+$password = ''; // Mot de passe vide par défaut sur XAMPP
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+    // Pour afficher les erreurs PDO
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion réussie à la base de données.";
+} catch (PDOException $e) {
+    echo "Erreur de connexion : " . $e->getMessage();
+}
+?>
