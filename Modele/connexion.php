ea3aa948ab01fefa39eac5c8e77a7198f8627ce7
Emily D'Harambure <cypr.jeux@gmail.com>
2025-09-17 11:52:56 +0200

    base

--- Modele/connexion.php
+++ Modele/connexion.php
@@ -1,6 +1,6 @@
 <?php
 $host = 'localhost'; // ou 127.0.0.1
-$dbname = 'workshop';
+$dbname = 'Workshop';
 $username = 'root';
 $password = ''; // Mot de passe vide par défaut sur XAMPP
 
@@ -13,3 +13,24 @@ try {
     echo "Erreur de connexion : " . $e->getMessage();
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Login</title>
+    <link rel="stylesheet" type="text/css" href="css/styles.css" /> 
+</head>
+<body>
+    <h2>Connexion</h2>
+    <form method="post">
+        <label>Nom d'utilisateur :</label>
+        <input type="text" name="username" required><br><br>
+
+        <label>Mot de passe :</label>
+        <input type="password" name="password" required><br><br>
+
+        <button type="submit">Se connecter</button>
+    </form>
+</body>
+</html>
